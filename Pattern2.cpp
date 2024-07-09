66eebf0d66d998670f3c3a36e07db5e0503dc1cc
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-07-09 22:31:52 +0530

    Add files via upload

---
+++ Pattern2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number of lines in the pattern:"<<endl;
+	cin>>n;
+	for(int row=1;row<=n;row++)
+	{
+	//for every row run the column
+	for(int col=1;col<=n;col++)
+	{
+	cout<<"*";
+	}
+	cout<<endl;
+	}
+	return 0;
+}
